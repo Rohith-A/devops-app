aebfdbb4bae93ce829f3a2b310d2ba0119159083
Rohith-A <rohithchintu1914@gmail.com>
2023-12-09 21:09:05 +0000

    allowing hosts

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-n6rv*ap=&+zj9+9ff+jg^s1qu5qe-=g6*$bf*oqbj@vyvwd+@t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
