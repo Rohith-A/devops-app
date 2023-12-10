081887d74d19f16c12b55d2dbe5a1972b31e102a
Rohith-A <rohithchintu1914@gmail.com>
2023-12-10 19:52:11 +0000

    run pylint

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -6,6 +6,10 @@ sudo rm -f /etc/nginx/sites-enabled/default
 
 sudo cp /home/ubuntu/devops-app/nginx/nginx.conf /etc/nginx/sites-available/blog
 sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/
+cd /home/ubuntu/devops-app/todo_project
+pylint *.py
+cd /home/ubuntu/devops-app/todo_app
+pylint *.py
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
 sudo gpasswd -a www-data ubuntu
